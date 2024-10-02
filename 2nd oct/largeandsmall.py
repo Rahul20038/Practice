cb78f7a5e68289f2834a9d346e96c9baf0529254
Rahul20038 <rahulchowdarynallamothu@gmail.com>
2024-10-02 11:22:30 +0530

    task completed

---
+++ 2nd oct/largeandsmall.py
@@ -0,0 +1,10 @@
+li = [10,2,5,9,15,56]
+#method-1
+#li.sort()
+#smallest = li[0]
+#largest = li[-1]
+#print(smallest)
+#print(largest)
+#method-2
+print("smallest number is",min(li))
+print("largest number is",max(li))
