514f2476410db00ae3b99421a073b005a3bbe017
Rahul20038 <rahulchowdarynallamothu@gmail.com>
2024-10-21 12:58:57 +0530

    Task completed

---
+++ 19th-oct/pattern1.py
@@ -0,0 +1,11 @@
+#pyramid
+def pyramid(n):
+    for i in range(n):
+        for j in range(n-i-1):
+            print(" ", end="")
+        for j in range(2*i+1):
+            print("*",end="")
+
+        print()
+n = 5
+print(pyramid(n))
