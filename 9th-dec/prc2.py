745597fb5ee40fa503349f327b8cabbc2919e38e
Rahul20038 <rahulchowdarynallamothu@gmail.com>
2024-12-09 10:06:10 +0530

    ss

--- 9th-dec/prc2.py
+++ 9th-dec/prc2.py
@@ -1,4 +1,4 @@
 def find_duplicates(lst):
-    duplicates = set([x for x in lst if lst.count(x > 1)])
+    duplicates = set([x for x in lst if lst.count(x) > 1])
     return list(duplicates)
-print(find_duplicates([9,9,5,9,2,6,5,1,9,8,1]))
+print(find_duplicates([1, 2, 3, 2, 4, 5, 1]))
